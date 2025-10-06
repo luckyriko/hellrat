a692711c6cf770437ff22e189ca890f754793838
理 <luckyriko@qq.com>
2025-10-06 20:50:43 +0800

    zip插件更新；App单例模式

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -78,6 +78,12 @@ fn on_window_event(window: &Window, event: &WindowEvent) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }))
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_http::init())
